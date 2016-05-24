d88d2007772ae00eb9231210c25e4d768741b236
IlyaGetReady <ilyagetready@gmail.com>
2016-05-24 13:49:55 +0100

    Tracer added

---
+++ tracer/tracer.c
@@ -0,0 +1,13 @@
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int main(){
+  return 0;
+}
