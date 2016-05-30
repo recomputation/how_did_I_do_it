9c6284a085c86695c9681e20bee9c11fa9f1a713
ilia.shumailov <is33@trenco.cs.st-andrews.ac.uk>
2016-05-30 10:38:33 +0100

    Updated the readme

--- utility/test_program.py
+++ utility/test_program.py
@@ -2,6 +2,11 @@ from random import randint
 
 rfile=open("cat_"+str(randint(0, 100)), "wb+")
 
-rfile.write("AAAFAFAF");
+read_file = open("test", "rb");
+
+for line in read_file:
+    print line
+
+rfile.write("AAAFAFAF" + str(randint(0, 1000)));
 
 rfile.close()
